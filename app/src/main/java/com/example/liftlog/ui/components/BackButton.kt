c96529049a5ece5b709a7432e5efd22af443e384
Marcus441 <88486170+Marcus441@users.noreply.github.com>
2026-07-26 10:47:09 +1000

    fix(ui): implement debouncing on BackButton component

--- app/src/main/java/com/example/liftlog/ui/components/BackButton.kt
+++ app/src/main/java/com/example/liftlog/ui/components/BackButton.kt
@@ -6,12 +6,25 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun BackButton(onBackClick: () -> Unit){
-    IconButton(onClick = onBackClick, Modifier.size(32.dp)) {
+fun BackButton(onBackClick: () -> Unit) {
+    var isClicked by remember {
+        mutableStateOf(false)
+    }
+
+    IconButton(onClick = {
+        if (!isClicked){
+            isClicked = true
+            onBackClick()
+        }
+    }, Modifier.size(32.dp)) {
         Icon(
             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
             contentDescription = "Navigate Back"
